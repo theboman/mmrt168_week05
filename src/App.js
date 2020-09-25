609cfd47ee0d228e154c185281acd4fa08d7f968
Bo Opfer <boopfer9k9@gmail.com>
2020-09-25 13:25:15 +0700

    homework week 5 react increment

--- src/App.js
+++ src/App.js
@@ -2,14 +2,40 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  let [count, setCount] = useState(0);
+  const [incrementBy, setIncrementBy] = useState(1);
+
+  function myCounter(type, multiple) {
+    if (count >= 10 || count <= -10) {
+      setCount((count = 0));
+    }
+    if (type === "add") {
+      setCount(count + multiple);
+    } else if (type === "subtract") {
+      setCount(count - multiple);
+    }
+  }
+
   return (
     <div className="App">
       <div className="counterContainer">
-        <div>Counter:</div>
-        <div>1</div>
+        <div className="counter">
+          Counter: {count} and increment is: {incrementBy}
+        </div>
+        <div className="buttonGroup">
+          {/* <button onClick={() => setCount(count + 1)}>+</button> */}
+          <button onClick={() => myCounter("add", incrementBy)}>+</button>
+          <button onClick={() => myCounter("subtract", incrementBy)}>-</button>
+          <button
+            onClick={() => {
+              incrementBy === 1 ? setIncrementBy(2) : setIncrementBy(1);
+            }}
+          >
+            {" "}
+            x2{" "}
+          </button>
+        </div>
       </div>
-      <button>+</button>
-      <button>-</button>
     </div>
   );
 }
